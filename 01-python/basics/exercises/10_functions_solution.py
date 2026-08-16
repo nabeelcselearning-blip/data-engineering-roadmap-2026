2d7efd60c6f95565d2f6be5b0109c22a38ec27b6
Nabeel Islam <nabeel.cse.learning@gmail.com>
2026-08-16 13:00:56 +0530

    Solved Lists remaining exercises and functions
    Learn new on this line:-
    shape_type = (
        input("Enter shape (triangle/rectangle): ") or "triangle"
    )
    if input is nothing it means empty string which is False then it will take triangle as value in variable shape_type.

---
+++ 01-python/basics/exercises/10_functions_solution.py
@@ -0,0 +1,25 @@
+# def calculate_area(base, height):
+#     area = 0.5 * base * height
+#     return area
+
+
+def calculate_area_2(base, height, shape_type="triangle"):
+
+    if shape_type == "triangle":
+        area = 0.5 * base * height
+
+    elif shape_type == "rectangle":
+        area = base * height
+
+    return area
+
+
+base = float(input("Enter base/length: "))
+height = float(input("Enter height/width: "))
+shape_type = (
+    input("Enter shape (triangle/rectangle): ") or "triangle"
+)  # new to me, if input is emty string thn by default it will store triangle in variable
+# A or B means in Python "If A has a value, use A. Otherwise, use B."
+
+# print(calculate_area(base, height))
+print(f"Area of {shape_type} is {calculate_area_2(base, height, shape_type)}")
--- 01-python/basics/exercises/9_for_solution.py
+++ 01-python/basics/exercises/9_for_solution.py
@@ -17,10 +17,56 @@ for elem in result:
     if elem == "heads":
         count_heads += 1
 
-print(f"We gor {count_heads} times heads")
+print(f"We got {count_heads} times heads")
 
 
 # SOLUTION 2:
 for i in range(11):
     if i % 2 != 0:
         print(i**2, end=" ")
+
+print()
+# SOLUTION 3:
+expense_list = [2340, 2500, 2100, 3100, 2980]
+expense_amount = int(input("Enter an expense amount: "))
+
+cond = False
+month = ""
+for i in range(5):
+    if expense_list[i] == expense_amount:
+        cond = True
+        if i == 0:
+            month = "January"
+        elif i == 1:
+            month = "February"
+        elif i == 2:
+            month = "March"
+        elif i == 3:
+            month = "April"
+        else:
+            month = "May"
+        break
+if cond:
+    print(f"{expense_amount} spend in {month}")
+else:
+    print(f"{expense_amount} is not spend from January to May")
+
+# SOLUTION 4:
+
+tired = False
+for i in range(5):
+    prompt = input("Are you tired?")
+    if prompt == "yes":
+        print("you didn't finish the race")
+        tired = True
+        break
+if not tired:
+    print("Congratulations u completed the race")
+
+# SOLUTION 5:
+
+for i in range(5):
+    for j in range(0, i + 1):
+        print("*", end="")
+
+    print()
