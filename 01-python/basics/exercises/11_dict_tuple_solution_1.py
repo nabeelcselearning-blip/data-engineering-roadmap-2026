b70aabe2f7996bfa89b6f095ac1caed6ba9612fc
Nabeel Islam <nabeel.cse.learning@gmail.com>
2026-08-16 23:44:16 +0530

    Add methods to do specific user task, and match case instead of conditions.
    Methods make code readable and concise.

--- 01-python/basics/exercises/11_dict_tuple_solution_1.py
+++ 01-python/basics/exercises/11_dict_tuple_solution_1.py
@@ -1,26 +1,55 @@
 country_population = {"China": 143, "India": 136, "USA": 32, "Pakistan": 21}
 
-user_input = input()
 
-if user_input == "print":
+def print_method():
     for key, value in country_population.items():
         print(f"{key} ==> {value}")
 
-elif user_input == "add":
+
+def add():
     country = input("Enter Country Name: ")  # case sensitive India != india
     if country not in country_population:
         population = int(input("Enter population"))
         country_population[country] = population
+        print(f"{country} ==> {population}")
     else:
         print("It already exists")
 
-elif user_input == "remove":
+
+def remove():
     country = input("Enter Country Name: ")
     if country in country_population:
         del country_population[country]
+        print_method()
     else:
         print("Country doesn't exist")
-elif user_input == "query":
+
+
+def query():
     country = input("Enter Country Name to Query: ")
     if country in country_population:
         print(f"The population of {country} is: {country_population[country]}")
+    else:
+        print("country not found.")
+
+
+print(
+    "Enter Your Choice:\n1.print: to print dict\n2.add: to add more country records.\n3.remove: to remove a country record.\n4.query to query from existing records. "
+)
+user_input = input()
+
+match user_input:
+    case "print":
+        print_method()
+
+    case "add":
+        add()
+
+    case "remove":
+        remove()
+
+    case "query":
+        query()
+
+    case _:
+        print("Invalid Input")
