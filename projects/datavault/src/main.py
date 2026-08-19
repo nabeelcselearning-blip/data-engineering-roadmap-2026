fbc5dc9baf1e751fffadfdb9f93f214e7113dba0
Nabeel Islam <259565683+nabeelcselearning-blip@users.noreply.github.com>
2026-08-19 07:09:55 +0530

    data processing/ add processed data into "transactions_processed.csv" file
    csv.DictWriter, writeheader(), writerow().

--- projects/datavault/src/main.py
+++ projects/datavault/src/main.py
@@ -1,11 +1,36 @@
 import csv
 
-with open("projects/datavault/data/raw/transactions.csv", "r") as f:
-    reader = csv.DictReader(f)
+with open("projects/datavault/data/raw/transactions.csv", "r") as rt:
+    raw_csv = csv.DictReader(rt)
 
-    for row in reader:
+    with open(
+        "projects/datavault/data/processed/transactions_processed.csv", "w"
+    ) as pt:
 
-        row["quantity"] = int(row["quantity"])
-        row["price"] = float(row["price"])
-        row["total_amount"] = row["quantity"] * row["price"]
-        print(row)
+        fields = [
+            "transaction_id",
+            "customer_id",
+            "product",
+            "quantity",
+            "price",
+            "date",
+            "total_amount",
+        ]
+        processed_csv = csv.DictWriter(pt, fieldnames=fields)
+
+        processed_csv.writeheader()
+
+        for row in raw_csv:
+
+            row["quantity"] = int(row["quantity"])
+            row["price"] = float(row["price"])
+            row["total_amount"] = row["quantity"] * row["price"]
+            print(row)
+
+            processed_csv.writerow(row)
+
+# with open("projects/datavault/data/processed/transactions_processed.csv", "r") as file:
+#     csv_file = csv.DictReader(file)
+
+#     for row in csv_file:
+#         print(row)
